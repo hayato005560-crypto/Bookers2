36a9686095e06522d7beb2b9d4349dfa8c1b72f7
Ubuntu <ubuntu@ip-172-31-40-88.ap-northeast-1.compute.internal>
2026-06-25 05:39:06 +0000

    いいね非同期機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,24 @@
 class FavoritesController < ApplicationController
   
     def create
-    book = Book.find(params[:book_id])
-    favorite = Current.user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_back(fallback_location: book_path(book))
+    @book = Book.find(params[:book_id])
+    Current.user.favorites.find_or_create_by(book_id: @book.id)
+    
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back(fallback_location: book_path(@book)) }
+    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = Current.user.favorites.find_by(book_id: book.id)
-    favorite.destroy if favorite
-    redirect_back(fallback_location: book_path(book))
+    @book = Book.find(params[:book_id])
+    favorite = Current.user.favorites.find_by(book_id: @book.id)
+    favorite&.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back(fallback_location: book_path(@book)) }
+    end
   end
   
 end
